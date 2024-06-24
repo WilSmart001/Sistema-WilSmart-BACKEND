ba68f71e61c41b43d7b12b7f3beaf347de39d0ac
Romulo Tole <tolemaicor@gmail.com>
2024-06-24 00:42:50 -0400

    Agregando port

--- src/app.py
+++ src/app.py
@@ -4,17 +4,21 @@ from config import config
 from routers import product, employee
 
 app = Flask(__name__)
-CORS(app, resources={r'*': {'origin': 'http//localhost:5173'}})
+CORS(app, resources={r'*': {'origin': 'http://localhost:5173'}})
 
 def page_not_found(error):
     return '<h1>Not found page</h1>', 404
 
-def run_flask_app():
-    app.config.from_object(config['development'])
+# Configuración de la aplicación Flask
+def create_app(config_name):
+    app.config.from_object(config[config_name])
     app.register_blueprint(product.main, url_prefix='/product')
     app.register_blueprint(employee.main, url_prefix='/employee')
     app.register_error_handler(404, page_not_found)
-    app.run(host='0.0.0.0')
+    return app
 
 if __name__ == '__main__':
-    run_flask_app()
+    # Aquí puedes ejecutar la aplicación directamente para pruebas locales
+    app = create_app('development')
+    app.run(host='0.0.0.0', port=5000)  # Puerto que prefieras para pruebas locales
+
